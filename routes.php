57a7a51e3607db96fd785d9509074d874963ba12
Josef Karlsson <josef.karlsson@lsbolagen.se>
2023-06-29 09:01:56 +0200

    Rename endpoint route

--- routes.php
+++ routes.php
@@ -3,5 +3,5 @@ use Api\Router;
 use Api\Endpoint\Order;
 use Api\Endpoint\Product;
 
-Router::get("/order/averageValue", fn() => Order::averageValue());
+Router::get("/orders/averageValue", fn() => Order::averageValue());
 Router::get("/products/mostPopular", fn() => Product::mostPopular());
