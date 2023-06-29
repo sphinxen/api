5f82f225a9478b2eb684b93d8fdb70ad492abaff
Josef Karlsson <josef.karlsson@lsbolagen.se>
2023-06-29 06:44:09 +0200

    Add endpoint mostPopularProducts

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ use Api\Endpoint\Stats;
 require_once __DIR__ . "/bootstrap.php";
 
 Router::get("/order/averageValue", fn() => Stats::averageOrderValue());
+Router::get("/products/mostPopular", fn() => Stats::mostPopularProducts());
 
 $method = $_SERVER['REQUEST_METHOD'];
 $path = $_SERVER['REQUEST_URI'];
--- src/Endpoint/Stats.php
+++ src/Endpoint/Stats.php
@@ -38,4 +38,45 @@ class Stats
 
         return ["averageOrderValue" => array_sum($orderValues) / count($orderValues)];
     }
+
+
+    public static function mostPopularProducts(int $nbrOfResults = 3)
+    {
+        $response = self::request("GET", "orders", [
+            "query" => [
+                "include_details" => "true",
+            ]
+        ]);
+
+        $productCount = [];
+        foreach(array_column($response, "products") as $poducts) {
+            foreach($poducts as $product) {
+                if (isset($productCount[$product->product_id])) {
+                    $productCount[$product->product_id]["count"] += (int)$product->qty;
+                } else {
+                    $productCount[$product->product_id]["count"] = $product->qty;
+                    $productCount[$product->product_id]["product"] = $product;
+
+                }
+            }
+        }
+
+        uasort($productCount, fn($p1, $p2) => $p2["count"] <=> $p1["count"]);
+
+        if ($_GET["include_details"] ?? false) {
+            return self::productDetails(array_keys($productCount));
+        }
+        return array_slice($productCount, 0 , $nbrOfResults);
+    }
+
+    private static function productDetails(array $product_ids)
+    {
+        $response = self::request("GET", "orders", [
+            "query" => [
+                "include_details" => "true",
+            ]
+        ]);
+
+        return json_decode($response->getBody()->getContents());
+    }
 }
