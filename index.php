91c0e1ed540d79b99b6b22be8bb798d1429e5a07
Josef Karlsson <josef.karlsson@lsbolagen.se>
2023-06-29 06:31:58 +0200

    Basic API setup

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
 
+use Api\Router;
+use Api\Endpoint\Stats;
+
 require_once __DIR__ . "/bootstrap.php";
+
+$method = $_SERVER['REQUEST_METHOD'];
+$path = $_SERVER['REQUEST_URI'];
+
+Router::dispatch($method, $path);
